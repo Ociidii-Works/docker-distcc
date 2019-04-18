742f4b5003856c2a19d0d575c9d274b255652ee3
XenHat <XenHat@users.noreply.github.com>
2019-04-18 13:39:44 -0400

    fix recreate script

--- host_scripts/recreate.cmd
+++ host_scripts/recreate.cmd
@@ -3,4 +3,4 @@ docker kill distcc-server-local
 docker rm distcc-server-local
 docker build . --tag=distcc-local
 rem docker run -p 3632:3632 --name distcc-server-local distcc-local
-docker-compose up --force-recreate --build
+docker-compose up --force-recreate --build --detach
